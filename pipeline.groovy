08e5386b9da71ade1b3b69035e2dd4dbbeb19ea2
vargadan <danielvrg@gmail.com>
2018-02-17 17:27:16 +0100

    x

--- pipeline.groovy
+++ pipeline.groovy
@@ -8,7 +8,7 @@ node('maven') {
    	def PROD_PROJECT = "ctr-prod"
    	def PORT = 8080
    	def GIT_URL = "https://github.com/vargadan/${APP_NAME}.git"
-   	def SKIP_TEST = "false"
+   	def SKIP_TEST = "true"
  
    	stage ('Build & Test') {
    		git branch: 'master', url: "${GIT_URL}"
@@ -22,19 +22,19 @@ node('maven') {
 	   	//tag for version in DEV imagestream
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${CICD_PROJECT}/${APP_NAME}:${version}"
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${DEV_PROJECT}/${APP_NAME}:latest"
-		envSetup(DEV_PROJECT, APP_NAME, 'latest', false)
+		envSetup(DEV_PROJECT, APP_NAME, 'latest', true)
 	}
 
    	stage ('Deploy to IT') {
         //put into IT imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${IT_PROJECT}/${APP_NAME}:latest"
-        envSetup(QA_PROJECT, APP_NAME, 'latest', false)
+        envSetup(QA_PROJECT, APP_NAME, 'latest', true)
 	}
 	
    	stage ('Deploy to PROD') {
         //put into PROD imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${PROD_PROJECT}/${APP_NAME}:latest"
-        //envSetup(QA_PROJECT, APP_NAME, 'latest', false)
+        envSetup(QA_PROJECT, APP_NAME, 'latest', false)
 	}
 
 }
@@ -47,9 +47,9 @@ def envSetup(project, appName, version, recreate) {
 	echo "GET_DC_OUT : ${GET_DC_OUT}"
 	if (recreate && GET_DC_OUT.contains(appName)) {
 		sh "oc delete deploymentconfig,service,routes -l app=${appName} -n ${project}"
+		sh "oc new-app ${appName}:${version} -n ${project}"
+   		sh "oc expose svc ${appName} -n ${project}"
    	}
-    	sh "oc new-app ${appName}:${version} -n ${project}"
-   	sh "oc expose svc ${appName} -n ${project}"
 }
 
 def version() {
