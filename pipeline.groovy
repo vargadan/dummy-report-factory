03090f4f0d335e26a00e0023e4076ae156609d25
vargadan <danielvrg@gmail.com>
2018-02-17 17:06:51 +0100

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,10 +2,10 @@ node('maven') {
 	def APP_NAME = "dummy-report-factory"
    	// define commands
    	def mvnCmd = "mvn -s configuration/maven-cicd-settings.xml"
-   	def CICD_PROJECT = "reportengine-cicd"
-   	def DEV_PROJECT = "reportengine-dev"
-   	def QA_PROJECT = "reportengine-qa"
-   	def PROD_PROJECT = "reportengine-prod"
+   	def CICD_PROJECT = "ctr-cicd"
+   	def DEV_PROJECT = "ctr-dev"
+   	def IT_PROJECT = "ctr-it"
+   	def PROD_PROJECT = "ctr-prod"
    	def PORT = 8080
    	def GIT_URL = "https://github.com/vargadan/${APP_NAME}.git"
    	def SKIP_TEST = "false"
@@ -22,24 +22,33 @@ node('maven') {
 	   	//tag for version in DEV imagestream
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${CICD_PROJECT}/${APP_NAME}:${version}"
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${DEV_PROJECT}/${APP_NAME}:latest"
-		envSetup(DEV_PROJECT, APP_NAME, 'latest')
+		envSetup(DEV_PROJECT, APP_NAME, 'latest', true)
 	}
 
-   	stage ('Deploy to QA') {
-     	timeout(time:10, unit:'MINUTES') {
-        		input message: "Promote to QA?", ok: "Promote"
-        }
-        //put into QA imagestream
+   	stage ('Deploy to IT') {
+        //put into IT imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${QA_PROJECT}/${APP_NAME}:latest"
-        envSetup(QA_PROJECT, APP_NAME, 'latest')
+        envSetup(QA_PROJECT, APP_NAME, 'latest', true)
+	}
+	
+   	stage ('Deploy to PROD') {
+        //put into PROD imagestream
+        sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${QA_PROJECT}/${APP_NAME}:latest"
+        //envSetup(QA_PROJECT, APP_NAME, 'latest', false)
 	}
 
 }
 
-def envSetup(project, appName, version) {
-	sh "oc delete buildconfig,deploymentconfig,service,routes -l app=${appName} -n ${project}"
-   	sh "oc new-app ${appName}:${version} -n ${project}"
-   	sh "oc expose svc ${appName} -n ${project}"
+def envSetup(project, appName, version, recreate) {
+	NEW_APP = sh (
+		script: "oc new-app ${appName}:${version} -n ${project}",
+		returnStdout: true
+	).trim()
+	echo "New app : ${NEW_APP}"
+	if (recreate) {
+		sh "oc delete deploymentconfig,service,routes -l app=${appName} -n ${project}"
+   		sh "oc expose svc ${appName} -n ${project}"
+   	}
 }
 
 def version() {
